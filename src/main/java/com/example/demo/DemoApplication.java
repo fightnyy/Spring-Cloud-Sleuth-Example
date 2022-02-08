477227d776bc086c6d65500519c0441520db66c6
Marcin Grzejszczak <mgrzejszczak@vmware.com>
2022-02-08 09:54:38 +0100

    Works

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,5 +1,15 @@
 package com.example.demo;
 
+import java.util.Set;
+
+import brave.baggage.BaggageField;
+import brave.baggage.BaggageFields;
+import brave.baggage.BaggagePropagationConfig;
+import brave.baggage.CorrelationScopeConfig;
+import brave.baggage.CorrelationScopeCustomizer;
+import brave.context.slf4j.MDCScopeDecorator;
+import brave.sampler.Sampler;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -17,4 +27,22 @@ public class DemoApplication {
         return new RestTemplate();
     }
 
+    // We want to control the way baggage fields are being set
+    @Bean
+    CorrelationScopeCustomizer mdcOverridingScopeCustomizer() {
+        return builder -> {
+            // We can't override [key] to flush on update so we need to remove the current entries
+            builder.clear();
+            // We're adding back the default ones related to tracing context
+            builder.add(CorrelationScopeConfig.SingleCorrelationField.create(BaggageFields.TRACE_ID));
+            builder.add(CorrelationScopeConfig.SingleCorrelationField.create(BaggageFields.SPAN_ID));
+            builder.add(CorrelationScopeConfig.SingleCorrelationField.create(BaggageFields.SAMPLED));
+            // We're setting the [key] baggage to flush on update
+            builder.add(CorrelationScopeConfig.SingleCorrelationField
+                    .newBuilder(BaggageField.create("key"))
+                    .flushOnUpdate()
+                    .build());
+
+        };
+    }
 }
--- src/main/java/com/example/demo/controller/Controller.java
+++ src/main/java/com/example/demo/controller/Controller.java
@@ -1,11 +1,12 @@
 package com.example.demo.controller;
 
-import brave.Span;
-import brave.Tracer;
 import brave.propagation.ExtraFieldPropagation;
 import com.example.demo.service.MyService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+
+import org.springframework.cloud.sleuth.Span;
+import org.springframework.cloud.sleuth.Tracer;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,11 +18,12 @@ public class Controller {
     private final Tracer tracer;
     private final MyService myService;
 
+    // I've rewritten this code to use the Sleuth abstraction
     @GetMapping("/")
     public String printTestString(){
         Long userId = 5L;
         String userIdKey = "key";
-        ExtraFieldPropagation.set(userIdKey, userId.toString());
+        tracer.createBaggage("key", userId.toString());
         Span span = tracer.currentSpan();
         span.tag(userIdKey, userId.toString());
         log.info("Controller Called");
